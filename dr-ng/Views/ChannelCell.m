2b7c549bcc0f25ac673bbb12c13a9e2997a9d9fc
Mikkel Gravgaard <mikkel@klokke.dk>
2015-01-04 15:58:38 +0100

    optimize performance by not using KVO'ed layouting

--- dr-ng/Views/ChannelCell.m
+++ dr-ng/Views/ChannelCell.m
@@ -15,32 +15,11 @@
     self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
     if (self) {
 
-        RACSignal *highlight = [[[[self rac_signalForSelector:@selector(setSelected:animated:)] map:^id(RACTuple *tuple) {
-            return tuple.first;
-        }] combineLatestWith:[self rac_signalForSelector:@selector(setHighlighted:animated:)]] map:^id(RACTuple *tuple) {
-            RACTupleUnpack(NSNumber *selected, RACTuple *highlightedT) = tuple;
-            NSNumber *highlighted = highlightedT.first;
-            return @(selected.boolValue || highlighted.boolValue);
-        }];
-
-        RAC(self.imageView,image) = [highlight map:^id(NSNumber *number) {
-            return number.boolValue ? [UIImage imageNamed:@"Images/station_icon_generic_selected"] : [UIImage imageNamed:@"Images/station_icon_generic"];
-        }];
-
+        self.textLabel.text = @"";
+        self.detailTextLabel.text = @"";
         self.textLabel.font = [UIFont channelName];
-        RAC(self.textLabel,textColor) = [highlight map:^id(NSNumber *number) {
-            return number.boolValue ? [UIColor whiteColor] : [UIColor colorWithWhite:0.17 alpha:1];
-        }];
-
 
         self.detailTextLabel.font = [UIFont nowPlaying];
-        RAC(self.detailTextLabel,textColor) = [highlight map:^id(NSNumber *number) {
-            UIColor *green = [UIColor colorWithRed:0.81
-                                             green:0.96
-                                              blue:0.67
-                                             alpha:1];
-            return number.boolValue ? green : [UIColor colorWithWhite:0.51 alpha:1];
-        }];
 
         self.backgroundView = [[UIImageView alloc] initWithImage:[UIImage new]];
         self.selectedBackgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Images/cell_bg_selected"]];
@@ -52,9 +31,36 @@
     return self;
 }
 
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
+    [super setHighlighted:highlighted animated:animated];
+    [self setNeedsLayout];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    [self setNeedsLayout];
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    BOOL highOrSel = self.highlighted || self.selected;
+    self.imageView.image = highOrSel ? [UIImage imageNamed:@"Images/station_icon_generic_selected"] : [UIImage imageNamed:@"Images/station_icon_generic"];
+
+    self.textLabel.textColor  = highOrSel ? [UIColor whiteColor] : [UIColor colorWithWhite:0.17 alpha:1];
+
+    self.detailTextLabel.textColor = highOrSel ? [UIColor colorWithRed:0.81
+                                                                 green:0.96
+                                                                  blue:0.67
+                                                                 alpha:1] :
+            [UIColor colorWithWhite:0.51 alpha:1];
+
+
+}
+
 - (void)configure:(Channel *)channel {
     self.textLabel.text = channel.name;
     self.detailTextLabel.text = channel.broadcaster;
+    [self setNeedsLayout];
 }
 
 @end
