f15b670ffba22a962531469327d60d946efbf935
Mikkel Gravgaard <mikkel@klokke.dk>
2015-01-04 15:20:15 +0100

    clean up

--- dr-ng/PlayerViewController.h
+++ dr-ng/PlayerViewController.h
@@ -4,16 +4,6 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "SPSession.h"
-static NSString *const kTracklistId = @"tracklistUrl";
-static NSString *const kFallbackTracklistId = @"fallbackId";
-
-@class SPPlaybackManager;
-@class AVPlayer;
-@class BTFSpotify;
-@class PlayerView;
-@class MessageView;
-
 
 @interface PlayerViewController : UIViewController
 @end
